8319ffe8b8656a8c95833cb1e6850cc37d017466
cruzayye <johnnyc010392@gmail.com>
2019-03-11 23:12:35 -0700

    removed node modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React, { PureComponent } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 
 
 
 export default class Demo extends PureComponent {
  
-
-
-
   render() {
     return (
       <>
       <Header />
+      <Footer />
       </>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer> <p>&copy; something smart goes here</p></footer>
+  );
+
+}

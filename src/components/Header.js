0e52871784c12f885e79022ffd2280180b898c5b
cruzayye <johnnyc010392@gmail.com>
2019-03-21 22:03:34 -0700

    added transitions to button and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,13 @@ import styles from './Header.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
+
 function Header() {
   return (
     <div className={styles.links}>
-      <Link to='/'> Home </ Link>
-      <Link to='/profile'> Profile </ Link>
-      <Link to='/followers'> Followers </ Link>
+      <Link to='/'> <p>Home</p> </ Link>
+      <Link to='/profile'> <p>Profile</p> </ Link>
+      <Link to='/followers'> <p>Followers</p>  </ Link>
     </div>  
     
   );
